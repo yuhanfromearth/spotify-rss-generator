a8db3ff69b7e0df93d8daca52d9fd9755f9619dc
ChiefYuHan <mail.yuhanmeyer@gmail.com>
2025-01-10 18:02:31 +0100

    fix gh actions run feed updater job, add debug logging

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 import staticRoutes from "./routes/routes.js";
 import feedRoutes from "./routes/feedRoutes.js";
 import dbService from "./services/database.js";
-import startScheduler from "./services/scheduler.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +22,6 @@ app.use("/", feedRoutes);
 
 try {
   await dbService.connect();
-  await startScheduler();
   app.listen(PORT, () => {
     console.log(`Spotify RSS Feed Generator listening on port ${PORT}`);
   });
